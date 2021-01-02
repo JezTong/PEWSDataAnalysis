23ad80de9dc40be2942c7ed0b7f3419bb6b78539
JezTong <72753905+JezTong@users.noreply.github.com>
2021-01-02 17:35:08 +0000

    Initial commit

---
+++ PEWS.py
@@ -0,0 +1,10 @@
+"""
+    NAME:          PEWSDataAnalysis
+    AUTHOR:        Jeremy Tong
+    EMAIL:         jeremy.tong.17@ucl.ac.uk
+    DATE:          02/01/2021
+    INSTITUTION:   University College London & University of Manchester
+    DESCRIPTION:   Python file for analysing PEWS Data for MSc Dissertation
+    DEPENDENCIES:  This program requires the following libraries:
+"""
+
