b55f7801025e3446d75ddddbbfe8de45ce3ec611
JezTong <72753905+JezTong@users.noreply.github.com>
2021-01-03 18:27:50 +0000

    Initial commit added workspace config files

--- PEWS.py
+++ PEWS.py
@@ -3,6 +3,7 @@
     AUTHOR:        Jeremy Tong
     EMAIL:         jeremy.tong.17@ucl.ac.uk
     DATE:          02/01/2021
+    VERSION:       0.1.0
     INSTITUTION:   University College London & University of Manchester
     DESCRIPTION:   Python file for analysing PEWS Data for MSc Dissertation
     DEPENDENCIES:  This program requires the following modules:
@@ -16,14 +17,14 @@ import pandas as pd
 import requests
 
 # Open the PEWS .csv file: replace 'my_PEWS_file.csv' with correct file path.
-with open('my_PEWS_file.csv') as PEWS_file:
+with open('TestFile.csv') as PEWS_file:
 
     # read the PEWS .csv file
     PEWS_raw_data = csv.reader(PEWS_file)
     for row in PEWS_raw_data:
         print(row)
 
-    # create a DataFrame form the PEWS data
+    # create a DataFrame form the PEWS data (can be excel file with .read_excel)
     PEWS_df = pd.read_csv(PEWS_file)
     # Read the first 10 rows
     PEWS_df.head(10)
