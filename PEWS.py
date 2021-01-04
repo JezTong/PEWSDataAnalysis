dc4b5dc975908aec80d8d2b07e4699ae2ed2d967
JezTong <72753905+JezTong@users.noreply.github.com>
2021-01-04 17:33:47 +0000

    Able to access test file on sharepoint and convert to a dataframe

--- PEWS.py
+++ PEWS.py
@@ -3,28 +3,77 @@
     AUTHOR:        Jeremy Tong
     EMAIL:         jeremy.tong.17@ucl.ac.uk
     DATE:          02/01/2021
-    VERSION:       0.1.0
+    VERSION:       0.2.0
     INSTITUTION:   University College London & University of Manchester
     DESCRIPTION:   Python file for analysing PEWS Data for MSc Dissertation
     DEPENDENCIES:  This program requires the following modules:
-                    csv, Numpy, Pandas, Requests
+                    io, csv, Numpy, Pandas, Requests, getpass
+                    Office365-REST-Python-Client 2.3.1 https://pypi.org/project/Office365-REST-Python-Client/
 """
 
 # Import Python Modules
+import io
 import csv
 import numpy as np
 import pandas as pd
 import requests
+import getpass
+
+# Import office365 share point API elements
+from office365.runtime.auth.user_credential import UserCredential
+from office365.runtime.auth.authentication_context import AuthenticationContext
+from office365.sharepoint.client_context import ClientContext
+from office365.runtime.client_request import ClientRequest
+from office365.sharepoint.files.file import File
+
+
+sharepoint_url = "https://uhltrnhsuk.sharepoint.com"
+username = input('username: ')
+password = getpass.getpass()
+site_url = "https://uhltrnhsuk.sharepoint.com/sites/case"
+folder_url = "/sites/case/Shared%20Documents/PEWSDataAnalysis/"
+file_url = "/sites/case/Shared%20Documents/PEWSDataAnalysis/PEWS_NC_Test_200.csv"
+filename = "PEWS_NC_TEST_200.csv"
+
+
+# Access sharepoint folder with authentication
+ctx_auth = AuthenticationContext(sharepoint_url)
+if ctx_auth.acquire_token_for_user(username, password):
+  request = ClientRequest(ctx_auth)
+  ctx = ClientContext(site_url, ctx_auth)
+  web = ctx.web
+  ctx.load(web)
+  ctx.execute_query()
+  print("Logged into: {0}".format(web.properties['Title']))
+
+else:
+  print (ctx_auth.get_last_error())
+
+
+# Download the file to memory
+response = File.open_binary(ctx, file_url)
+# save data to BytesIO stream
+bytes_file_obj = io.BytesIO()
+bytes_file_obj.write(response.content)
+bytes_file_obj.seek(0)  # set file object to start
+
+# create a DataFrame form the PEWS data (can be excel file with .read_excel)
+PEWS_df = pd.read_csv(bytes_file_obj)
+print(PEWS_df.head(10))
+
 
 # Open the PEWS .csv file: replace 'my_PEWS_file.csv' with correct file path.
-with open('TestFile.csv') as PEWS_file:
+# with open(filename, 'wb') as PEWS_file:
+#     response = File.open_binary(ctx, file_url)
+#     print(response)
+#     PEWS_file.write(response.content)
+#     print(PEWS_file)
+#     # read the PEWS .csv file
+#     PEWS_raw_data = csv.read    (PEWS_file)
+#     for row in PEWS_raw_data:
+#         print(row)
 
-    # read the PEWS .csv file
-    PEWS_raw_data = csv.reader(PEWS_file)
-    for row in PEWS_raw_data:
-        print(row)
-
-    # create a DataFrame form the PEWS data (can be excel file with .read_excel)
-    PEWS_df = pd.DataFrame(PEWS_file)
+#     # create a DataFrame form the PEWS data (can be excel file with .read_excel)
+#     PEWS_df = pd.DataFrame(PEWS_file)
     # Read the first 10 rows
-    print(PEWS_df.head(10))
+    # print(PEWS_df.head(10))
