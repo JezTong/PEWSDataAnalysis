f0d13c4d9d0e2acf905a6980b54beeeb93c4bd98
JezTong <72753905+JezTong@users.noreply.github.com>
2021-03-02 23:21:49 +0000

    Prepared load file for all 4 files and merge to one DataFrame. Built a class for thresholds

--- PEWS.py
+++ PEWS.py
@@ -38,7 +38,6 @@ username = input('username: ')
 password = getpass.getpass()
 site_url = "https://uhltrnhsuk.sharepoint.com/sites/case"
 folder_url = "/sites/case/Shared%20Documents/PEWSDataAnalysis/"
-filename = input("\nEnter file name: ")
 
 # Access sharepoint folder with authentication
 ctx_auth = AuthenticationContext(sharepoint_url)
@@ -64,26 +63,39 @@ def Load_file(filename):
   # set file object to start
   bytes_file_obj.seek(0)
 
-  global PEWS_df
+  global df
 
   if filename.endswith(".xlsx"):
-    print('\nLoading excel file...')
-    PEWS_dict = pd.read_excel(bytes_file_obj, sheet_name='Sheet1')  # read the excel file in python as a dictonary
-    PEWS_df = pd.DataFrame.from_dict(PEWS_dict)                     # convert dictionary to a dataframe using pandas
-    return PEWS_df
+    print('\nLoading {}...'.format(filename))
+    dict = pd.read_excel(bytes_file_obj, sheet_name='Sheet1')  # read the excel file in python as a dictonary
+    df = pd.DataFrame.from_dict(dict)                     # convert dictionary to a dataframe using pandas
+    return df
 
   elif filename.endswith(".csv"):
-    print("\nLoading .csv file...")
-    PEWS_df = pd.read_csv(bytes_file_obj)    # read the .csv file in python as a dataframe
-    return PEWS_df
+    print("\nLoading {}...".format(filename))
+    df = pd.read_csv(bytes_file_obj)    # read the .csv file in python as a dataframe
+    return df
 
   else:
     file = input("\nFile not recognised, please try again: ")
     Load_file(file)
 
-# load the sharepoint file specified by user
-Load_file(filename)
+""" Load the Sharepoint Files """
 
+PEWS_df = Load_file('PEWS_Data_1.xlsx')
+# HISS_df = Load_file('HISS_Data_1.xlsx')
+
+# Load all 4 data files on Sharepoint
+# PEWS_df_1 = Load_file('PEWS_Data_1.xlsx')
+# PEWS_df_2 = Load_file('PEWS_Data_2.xlsx')
+# PEWS_df = pd.concat([PEWS_df_1, PEWS_df_2])
+
+# HISS_df_1 = Load_file('HISS_Data_1.xlsx')
+# HISS_df_2 = Load_file('HISS_Data_2.xlsx')
+# HISS_df = pd.concat([HISS_df_1, HISS_df_2])
+
+# Merge the PEWS and HISS Data files
+# full_df = pd.merge(PEWS_df, HISS_df, on='spell_id', how='outer')
 
 
 """ Data Cleaning """
@@ -91,12 +103,14 @@ Load_file(filename)
 # explore and examine the DataFrame
 print('\nDisplaying DataFrame column headers and data types:\n')
 print(PEWS_df.dtypes)
-# print(PEWS_df)
-# print('\n')
 # print(PEWS_df.describe())
 # print(PEWS_df.head(10))
-# columns = PEWS_df.columns.tolist()
+# print('\n')
 
+# Clean the PEWS Heart Rate Data
+PEWS_df.HR = PEWS_df['HR'].replace('\D+', np.NaN, regex = True)   # replace text with null
+PEWS_df.HR = pd.to_numeric(PEWS_df.HR)                            # convert to python float/int
+PEWS_df.HR = PEWS_df['HR'].dropna()                               # remove null values
 
 # Function to clean the PEWS parameter Data
 # def data_clean(PEWS_df):
@@ -115,81 +129,56 @@ print(PEWS_df.dtypes)
 # data_clean(observation)
 
 
-# Clean the PEWS Data
-PEWS_df.HR = PEWS_df['HR'].replace('\D+', np.NaN, regex = True)   # replace text with null
-PEWS_df.HR = pd.to_numeric(PEWS_df.HR)                            # convert to python float/int
-PEWS_df.HR = PEWS_df['HR'].dropna()                               # remove null values
-Heart_Rates = PEWS_df['HR'].values
-
-
-print(PEWS_df.dtypes)
-# print(PEWS_df.describe())
-print('\n')
-
-
 """ Bin Data by age """
 
 PEWS_bins = [0, 1, 5, 12, 18]                          # Age bins according to UHL PEWS chart categories
 PEWS_bin_labels = ['0-11m', '1-4y', '5-11y', '>12']    # Age bin category labels
 
 # classify age according to age bins and add an Age bin column to the PEWS Dataframe
-PEWS_df['PEWS_bins'] = pd.cut(PEWS_df['Age at Obs'], PEWS_bins, labels = PEWS_bin_labels)
+PEWS_df['PEWS_bins'] = pd.cut(PEWS_df.age, PEWS_bins, labels = PEWS_bin_labels)
 
+
+""" Select the Heart Rate Data """
+
+Heart_Rates = PEWS_df[['HR', 'PEWS_bins', 'age', 'obs_sequence']].values
+Heart_Rates = pd.DataFrame(Heart_Rates)
 
 """ Data Analysis """
 
-# Seaborn method of plotting histogram
-# plt.clf()
-# sns.distplot(Heart_Rates)
-# plt.show()
+# Get the Heart rate data for each PEWS Age bin
+HR_PEWS_bin_1 = PEWS_df.loc[PEWS_df['PEWS_bins'] == '0-11m']
+HR_PEWS_bin_2 = PEWS_df.loc[PEWS_df['PEWS_bins'] == '1-4y']
+HR_PEWS_bin_3 = PEWS_df.loc[PEWS_df['PEWS_bins'] == '5-11y']
+HR_PEWS_bin_4 = PEWS_df.loc[PEWS_df['PEWS_bins'] == '>12']
 
-# Mathplotlib method test
-# plt.clf()
-# PEWS_df.plot(x="Age at Obs", y=["HR"])
-# plt.show()
+HR_PEWS_series = [HR_PEWS_bin_1.HR, HR_PEWS_bin_2.HR, HR_PEWS_bin_3.HR, HR_PEWS_bin_4.HR]
 
-HR_PEWS_bin_one = PEWS_df.loc[PEWS_df['PEWS_bins'] == '0-11m']
-HR_PEWS_bin_two = PEWS_df.loc[PEWS_df['PEWS_bins'] == '1-4y']
-HR_PEWS_bin_three = PEWS_df.loc[PEWS_df['PEWS_bins'] == '5-11y']
-HR_PEWS_bin_four = PEWS_df.loc[PEWS_df['PEWS_bins'] == '>12']
-
-HR_PEWS_series = [HR_PEWS_bin_one.HR, HR_PEWS_bin_two.HR, HR_PEWS_bin_three.HR, HR_PEWS_bin_four.HR]
-
-# Test plot the HR histograms
-# Title = '{0} distribution for {1}'.format('HR', age_bin_labels[0])
+# Plot the HR by PEWS age bins as a histogram
 Title = 'Heart rate distribution for 4 age groups'
 Range = (50, 200)
 # Range = (0, 250)
-# plt.clf()
-# plt.hist(HR_PEWS_bin_one.HR, range = Range, bins = 50, alpha = 0.4, label = PEWS_bin_labels[0] ) # edgecolor = 'white'
-# plt.hist(HR_PEWS_bin_two.HR, range = Range, bins = 50, alpha = 0.4, label = PEWS_bin_labels[1])
-# plt.hist(HR_PEWS_bin_three.HR, range = Range, bins = 50, alpha = 0.4, label = PEWS_bin_labels[2])
-# plt.hist(HR_PEWS_bin_four.HR, range = Range, bins = 50, alpha = 0.4, label = PEWS_bin_labels[3])
-# plt.title(Title)
-# plt.legend()
+plot1 = plt.figure(1)
+for i in range(len(HR_PEWS_series)):
+  plt.hist(HR_PEWS_series[i], range = Range, bins = 50, alpha = 0.4, label = PEWS_bin_labels[i])
+plt.title(Title)
+plt.legend()
 # plt.show()
 
 # Boxplot of Heart Rate for 4 age bins
-plt.clf()
+plot2 = plt.figure(2)
 plt.title('Heart rates for 4 different age groups')
 sns.boxplot(x = PEWS_df.PEWS_bins, y = PEWS_df.HR, width = 0.3)
-plt.show()
+# plt.show()
 
 # Boxplots of Heart Rate for each age
-plt.clf()
+plot3 = plt.figure(3)
 plt.title('Heart rate range at different ages')
-sns.boxplot(x = PEWS_df['Age at Obs'], y = PEWS_df.HR, width = 0.2)
+sns.boxplot(x = PEWS_df.age, y = PEWS_df.HR, width = 0.2)
 plt.show()
 
-# explore the Heart Rate data and plot a histogram of heart rates
-# HR_min = np.amin(PEWS_df.HR)
-# HR_max = np.amax(PEWS_df.HR)
-# range = HR_max - HR_min
-# print('\nThe lowest heart rate is {:.2f} beats per min'.format(HR_min))
-# print('\nThe highest heart rate is {:.2f} beats per min'.format(HR_max))
+""" Experimenting with adding PEWS Thresholds in plots"""
 
-HR_centiles = np.quantile(PEWS_df['HR'], [0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95])
-
+# HR_centiles = np.quantile(PEWS_df['HR'], [0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95])
 # centile_line_color = ['red', 'orange', 'yellow', 'green', 'yellow', 'orange', 'red']
 # plt.clf()
 # plt.hist(PEWS_df['HR'], range = (0, 250), bins = 25, edgecolor = 'white')
@@ -211,6 +200,40 @@ HR_centiles = np.quantile(PEWS_df['HR'], [0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95]
 # plt.show()
 
 
+""" UHL PEWS Model Thresholds """
+
+class PEWS_thresholds(object):
+
+  bin_1 = []
+  bin_2 = []
+  bin_3 = []
+  bin_4 = []
+
+  def __init__(self):
+    self.bin_1 = [
+      ['HR', 20, 90, 1],
+      ['HR', 91, 160, 0],
+      ['HR', 161, 310, 1]
+    ]
+
+    self.bin_2 = [
+      ['HR', 20, 90, 1],
+      ['HR', 91, 140, 0],
+      ['HR', 141, 310, 1]
+    ]
+
+    self.bin_3 = [
+      ['HR', 20, 70, 1],
+      ['HR', 71, 120, 0],
+      ['HR', 121, 250, 1]
+    ]
+
+    self.bin_4 = [
+      ['HR', 20, 60, 1],
+      ['HR', 61, 100, 0],
+      ['HR', 101, 250, 1]
+
+    ]
 
 
-
+exit()
