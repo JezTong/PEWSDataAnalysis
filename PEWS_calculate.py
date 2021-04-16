741e991ecaa7c3d91597973477db1c27232a62be
JezTong <72753905+JezTong@users.noreply.github.com>
2021-04-16 23:00:31 +0100

    Started code to calculate PEWS (WIP)

--- PEWS_calculate.py
+++ PEWS_calculate.py
@@ -11,9 +11,41 @@
 """
 
 import pandas as pd
-import PEWS_thresholds as PT
+import PEWS_models as PT
+
+""" Import Synthetic Observations dataset """
+
+df = pd.read_csv('Data/synthetic_obs.csv')
+
+""" Data Exploring """
+print('\nDisplaying DataFrame Summary:\n')
+
+# set pandas options to display all columns in a DataFrame
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
+# explore and examine the DataFrame
+print(df.describe())
+# print(df.head(10))
+
+# print('\nDisplaying DataFrame column headers and data types:\n')
+# print(df.dtypes)
+# print('\n')
+
+""" Calculate PEWS scores for different models """
 
 # activate PEWS threshold
 
+""" Calculate the centiles """
 
+# take in a DataFrame of the parameter in question
 
+# create a new column for the centile
+
+# select an an age band 'window'
+
+# take the window and work out its mean and standard deviation
+
+# calculate the sample size for this window (determine what the minimum sample size should be)
+
+# calculate the centile for each age using the 'window' with 'acceptable limits'
