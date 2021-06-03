90fd2b0c89986e75c779bf1064193d2dcab89138
JezTong <72753905+JezTong@users.noreply.github.com>
2021-06-03 11:07:12 +0100

    Update to the NPEWS model thresholds

--- PEWS_models.py
+++ PEWS_models.py
@@ -23,40 +23,39 @@ class NPEWS(object):
     def __init__(self):
         self.thresholds = [
 
-            # age ranges: 0 = 'all_ages', 1 = '0-11m', 2 = '1-4y', 3 = '5-11y', 4 = '>12y'
+            [0, 'concern', '', 'w', 0, False],
+            [0, 'concern', '', 's', 0, False],
+            [0, 'concern', '', 'b', 0, False],
+            [0, 'concern', '', 'pa', 0, False],
+            [0, 'concern', '', 'a', 0, False],
 
-            [0, 'concern', '', 'Worse', 0, False],
-            [0, 'concern', '', 'Same', 0, False],
-            [0, 'concern', '', 'Better', 0, False],
-            [0, 'concern', '', 'Parent Away', 0, False],
-            [0, 'concern', '', 'Parent Asleep', 0, False],
-
-            [1, 'RR', 81, 100, 4, False],
-            [1, 'RR', 71, 80, 2, False],
-            [1, 'RR', 61, 70, 1, False],
-            [1, 'RR', 41, 60, 0, True],
+            [1, 'RR', 80, 150, 4, False],
+            [1, 'RR', 70, 79, 2, False],
+            [1, 'RR', 60, 69, 1, False],
+            [1, 'RR', 41, 59, 0, True],
             [1, 'RR', 31, 40, 1, True],
             [1, 'RR', 21, 30, 2, False],
             [1, 'RR', 0, 20, 4, False],
 
-            [2, 'RR', 81, 100, 4, False],
-            [2, 'RR', 71, 80, 2, False],
-            [2, 'RR', 51, 70, 1, False],
-            [2, 'RR', 31, 50, 0, True],
+            [2, 'RR', 80, 150, 4, False],
+            [2, 'RR', 70, 79, 2, False],
+            [2, 'RR', 50, 69, 1, False],
+            [2, 'RR', 31, 49, 0, True],
             [2, 'RR', 21, 30, 1, True],
             [2, 'RR', 11, 20, 2, False],
             [2, 'RR', 0, 10, 4, False],
 
-            [3, 'RR', 41, 100, 4, False],
-            [3, 'RR', 31, 40, 2, False],
-            [3, 'RR', 26, 30, 1, False],
-            [3, 'RR', 16, 25, 0, True],
-            [3, 'RR', 11, 15, 1, True],
+            [3, 'RR', 60, 100, 4, False],
+            [3, 'RR', 50, 59, 2, False],
+            [3, 'RR', 40, 49, 1, False],
+            [3, 'RR', 21, 39, 0, True],
+            [3, 'RR', 16, 20, 1, True],
+            [3, 'RR', 11, 15, 2, False],
             [3, 'RR', 0, 10, 4, False],
 
-            [4, 'RR', 41, 100, 4, False],
-            [4, 'RR', 31, 40, 2, False],
-            [4, 'RR', 26, 30, 1, False],
+            [4, 'RR', 40, 100, 4, False],
+            [4, 'RR', 30, 39, 2, False],
+            [4, 'RR', 26, 29, 1, False],
             [4, 'RR', 16, 25, 0, True],
             [4, 'RR', 11, 15, 1, True],
             [4, 'RR', 0, 10, 4, False],
@@ -71,31 +70,34 @@ class NPEWS(object):
             [0, 'Sats', 95, 100, 0, False],
 
             [0, 'FiO2', 50, 100, 4, False],
-            [0, 'FiO2', 22, 49, 1, False],
+            [0, 'FiO2', 22, 49, 2, False],
             [0, 'FiO2', 21, 21, 0, False],
-            [0, 'O2liters', 4, 15, 4, False],
-            [0, 'O2litres', 0.1, 4, 1, False],
+            [0, 'O2liters', 4, 40, 4, False],
+            [0, 'O2litres', 0.01, 3.99, 2, False],
+            [0, 'O2Delivery', '', 'np', 2],
+            [0, 'O2Delivery', '', 'fm', np.NaN],
+            [0, 'O2Delivery', '', 'hb', np.NaN],
 
-            [1, 'HR', 181, 300, 4, False],
-            [1, 'HR', 171, 180, 2, True],
-            [1, 'HR', 151, 170, 1, True],
-            [1, 'HR', 111, 150, 0, True],
+            [1, 'HR', 180, 300, 4, False],
+            [1, 'HR', 170, 179, 2, True],
+            [1, 'HR', 150, 169, 1, True],
+            [1, 'HR', 111, 149, 0, True],
             [1, 'HR', 91, 110, 1, True],
             [1, 'HR', 81, 90, 2, True],
             [1, 'HR', 0, 80, 4, False],
 
-            [2, 'HR', 171, 300, 4, False],
-            [2, 'HR', 151, 170, 2, True],
-            [2, 'HR', 121, 150, 1, True],
-            [2, 'HR', 91, 120, 0, True],
+            [2, 'HR', 170, 300, 4, False],
+            [2, 'HR', 150, 169, 2, True],
+            [2, 'HR', 120, 149, 1, True],
+            [2, 'HR', 91, 119, 0, True],
             [2, 'HR', 71, 90, 1, True],
             [2, 'HR', 61, 70, 2, True],
             [2, 'HR', 0, 60, 4, False],
 
-            [3, 'HR', 161, 300, 4, False],
-            [3, 'HR', 141, 160, 2, True],
-            [3, 'HR', 121, 140, 1, True],
-            [3, 'HR', 81, 120, 0, True],
+            [3, 'HR', 160, 300, 4, False],
+            [3, 'HR', 140, 159, 2, True],
+            [3, 'HR', 120, 139, 1, True],
+            [3, 'HR', 81, 119, 0, True],
             [3, 'HR', 71, 80, 1, True],
             [3, 'HR', 61, 70, 2, True],
             [3, 'HR', 0, 60, 4, False],
@@ -108,34 +110,34 @@ class NPEWS(object):
             [4, 'HR', 51, 60, 2, True],
             [4, 'HR', 0, 50, 4, False],
 
-            [1, 'sBP', 111, 250, 4, False],
-            [1, 'sBP', 101, 110, 2, True],
-            [1, 'sBP', 91, 100, 1, True],
-            [1, 'sBP', 71, 90, 0, True],
+            [1, 'sBP', 110, 250, 4, False],
+            [1, 'sBP', 100, 109, 2, True],
+            [1, 'sBP', 90, 99, 1, True],
+            [1, 'sBP', 71, 89, 0, True],
             [1, 'sBP', 61, 70, 1, True],
             [1, 'sBP', 51, 60, 2, True],
             [1, 'sBP', 0, 50, 4, False],
 
-            [2, 'sBP', 131, 250, 4, False],
-            [2, 'sBP', 121, 130, 2, False],
-            [2, 'sBP', 101, 120, 1, False],
-            [2, 'sBP', 81, 100, 0, False],
+            [2, 'sBP', 130, 250, 4, False],
+            [2, 'sBP', 120, 129, 2, False],
+            [2, 'sBP', 100, 119, 1, False],
+            [2, 'sBP', 81, 99, 0, False],
             [2, 'sBP', 61, 80, 1, False],
             [2, 'sBP', 51, 60, 2, False],
             [2, 'sBP', 0, 50, 4, False],
 
-            [3, 'sBP', 141, 250, 4, False],
-            [3, 'sBP', 131, 140, 2, False],
-            [3, 'sBP', 121, 130, 1, False],
-            [3, 'sBP', 101, 120, 0, False],
-            [3, 'sBP', 91, 100, 1, False],
-            [3, 'sBP', 81, 90, 2, False],
-            [3, 'sBP', 0, 80, 4, False],
+            [3, 'sBP', 140, 250, 4, False],
+            [3, 'sBP', 120, 139, 2, False],
+            [3, 'sBP', 110, 119, 1, False],
+            [3, 'sBP', 91, 109, 0, False],
+            [3, 'sBP', 81, 90, 1, False],
+            [3, 'sBP', 71, 80, 2, False],
+            [3, 'sBP', 0, 70, 4, False],
 
-            [4, 'sBP', 141, 250, 4, False],
-            [4, 'sBP', 131, 140, 2, False],
-            [4, 'sBP', 121, 130, 1, False],
-            [4, 'sBP', 101, 120, 0, False],
+            [4, 'sBP', 140, 250, 4, False],
+            [4, 'sBP', 130, 139, 2, False],
+            [4, 'sBP', 120, 129, 1, False],
+            [4, 'sBP', 101, 119, 0, False],
             [4, 'sBP', 91, 100, 1, False],
             [4, 'sBP', 81, 90, 2, False],
             [4, 'sBP', 0, 80, 4, False],
@@ -143,19 +145,19 @@ class NPEWS(object):
             [0, 'cap_refill', 3, 10, 2, False],
             [0, 'cap_refill', 0, 2, 0, False],
 
-            [0, 'temp', 38.1, 44.0, 0, False],
-            [0, 'temp', 36.1, 38, 0, False],
+            [0, 'temp', 38.0, 44.0, 0, False],
+            [0, 'temp', 36.1, 37.9, 0, False],
             [0, 'temp', 0, 36.0, 0, False],
 
             [0, 'Sepsis6', 'N', 'Y', 0, False],
 
-            [0, 'AVPU', '', 'unresponsive', 0, False],
-            [0, 'AVPU', '', 'pain', 0, False],
-            [0, 'AVPU', '', 'voice', 0, False],
-            [0, 'AVPU', '', 'alert', 0, False],
-            [0, 'AVPU', '', 'asleep', 0, False],
+            [0, 'AVPU', '', 'u', 0, False],
+            [0, 'AVPU', '', 'p', 0, False],
+            [0, 'AVPU', '', 'v', 0, False],
+            [0, 'AVPU', '', 'a', 0, False],
+            [0, 'AVPU', '', 'as', 0, False],
 
-            [0, 'pain', '', 'no', 0, False],
+            [0, 'pain', '', 'none', 0, False],
             [0, 'pain', '', 'mild', 0, False],
             [0, 'pain', '', 'moderate', 0, False],
             [0, 'pain', '', 'severe', 0, False],
@@ -165,6 +167,13 @@ class NPEWS(object):
 
     pass
 
+
+"""
+# age ranges: { 0: 'all_ages', 1: '0-11m', 2: '1-4y', 3: '5-11y', 4: '>12y' }
+# concern = { 'w': 'Worse , 's': 'Same', 'b': 'Better', 'pa': 'Parent Away', 'a': 'Parent Asleep' }
+# AVPU = { 'asleep':'asleep', 'a':'alert','v':'responsive to voice', 'p': 'responsive to pain', 'u': 'unresponsive' }
+
+"""
 
 
 """ UHL Nervecentre PEWS model """
@@ -296,7 +305,7 @@ class UHL_PEWS(object):
 # Function to generate a table containing the PEWS model
 def generate_model_table(model):
     columns = ['age_range', 'parameter', 'lower_lim', 'upper_lim', 'score', 'plot?']
-    age_bin_labels = ['all_ages', '0-11m', '1-4y', '5-11y', '>12y']
+    age_range_labels = ['all_ages', '0-11m', '1-4y', '5-11y', '>12y']
 
     if model == 'UHL_PEWS':
         table = pd.DataFrame(UHL_PEWS().thresholds, columns=columns)
@@ -304,10 +313,23 @@ def generate_model_table(model):
     elif model == 'NPEWS':
         table = pd.DataFrame(NPEWS().thresholds, columns=columns)
 
-    table.age_range = table.age_range.apply(lambda x: age_bin_labels[x])
+    table.age_range = table.age_range.apply(lambda x: age_range_labels[x])
     table = table[['age_range', 'parameter', 'lower_lim', 'upper_lim', 'score']]
 
     return table
+
+""" view threshold tables and generate .csv files """
+
+# print(generate_model_table('UHL_PEWS'))
+# print('\n')
+print(generate_model_table('NPEWS'))
+generate_model_table('NPEWS').to_csv('data/NPEWS_limits.csv', index=False)
+# generate_model_table('UHL_PEWS').to_csv('data/UHL_PEWS_limits.csv', index=False)
+
+exit()
+
+
+
 
 
 # Function to generate a table containing the PEWS model limits (better for plotting)
@@ -382,6 +404,10 @@ def get_thresolds(model, parameter):
 
     return limits
 
+
+
+
+""" Code no longer used """
 # Function to generate threshold limit line coordinates for plotting in a chart
 def generate_lines(model, parameter, color = 'red', linewidth = 1):
     age_brackets = [0, 0, 1, 5, 12, 18]
@@ -403,11 +429,3 @@ def generate_lines(model, parameter, color = 'red', linewidth = 1):
 
     return  LineCollection(line_list, linewidth=linewidth, color=color)
 
-
-""" view threshold tables and generate .csv files """
-
-# print(generate_model_table('UHL_PEWS'))
-# print('\n')
-# print(generate_model_table('NPEWS'))
-# generate_model_table('NPEWS').to_csv('data/NPEWS_limits.csv', index=False)
-# generate_model_table('UHL_PEWS').to_csv('data/UHL_PEWS_limits.csv', index=False)
